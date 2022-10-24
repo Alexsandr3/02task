4d05095235ec1140986e75c75e7a672533860995
Alexsandr3 <fam_86@mail.ru>
2022-10-24 08:59:42 +0200

    fix posts-db-repositories 01

--- src/repositories/posts-db-repositories.ts
+++ src/repositories/posts-db-repositories.ts
@@ -38,11 +38,11 @@ export const postsRepositories ={
         return post.matchedCount === 1
     },
     async deletePostById (postId: string): Promise<boolean> {
-        const post = await postsCollection.deleteOne({postId})
+        const post = await postsCollection.deleteOne({id: postId})
         if (!post) {
             return false
         }
-        await postsCollection.findOne({postId})
+        await postsCollection.find({id: {$not: {postId}}})
         return true
     },
     async findPosts(): Promise<postsType[]> {
