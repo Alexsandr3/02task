3823c09d710071f6ef5bbae3794d58e8db97ce4b
Alexsandr3 <fam_86@mail.ru>
2022-10-24 08:46:54 +0200

    fix posts-db-repositories

--- src/repositories/posts-db-repositories.ts
+++ src/repositories/posts-db-repositories.ts
@@ -10,7 +10,7 @@ export  type postsType = {
     createdAt: string
 }
 
-let posts: postsType[] = []
+//let posts: postsType[] = []
 
 export const postsRepositories ={
     async createPost (title: string, shortDescription: string, content: string, blogId: string): Promise<postsType | null> {
@@ -30,20 +30,20 @@ export const postsRepositories ={
         await postsCollection.insertOne({...newPost})
         return newPost
     },
-    async findByIdPost (id: string): Promise<postsType | null> {
-        return await postsCollection.findOne({id},{projection: {_id: false}})
+    async findByIdPost (postId: string): Promise<postsType | null> {
+        return await postsCollection.findOne({id: postId},{projection: {_id: false}})
     },
     async updatePostById (postId: string, title: string, shortDescription: string, content: string, blogId: string): Promise<boolean>{
         const post = await postsCollection.updateOne({id:postId},{$set: {title: title, shortDescription: shortDescription, content: content, blogId: blogId}})
         return post.matchedCount === 1
     },
-    async deletePostById (id: string): Promise<postsType | null> {
-        const post = await postsCollection.deleteOne({id})
+    async deletePostById (postId: string): Promise<boolean> {
+        const post = await postsCollection.deleteOne({postId})
         if (!post) {
-            return null
+            return false
         }
-        return await postsCollection.findOne({id})
-
+        await postsCollection.findOne({postId})
+        return true
     },
     async findPosts(): Promise<postsType[]> {
         return postsCollection.find({}, {projection: {_id: false}}).toArray()
