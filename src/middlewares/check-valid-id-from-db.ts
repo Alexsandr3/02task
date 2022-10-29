48fc6df55d8fa9f3849dda6b063da70ecf31fd3c
Alexsandr3 <fam_86@mail.ru>
2022-10-29 10:15:21 +0200

    more fixes (error01) by task04

--- src/middlewares/check-valid-id-from-db.ts
+++ src/middlewares/check-valid-id-from-db.ts
@@ -2,8 +2,8 @@ import {NextFunction,Request, Response} from "express";
 import {ObjectId} from "mongodb";
 
 export const checkIdValidForMongodb = (req: Request, res: Response, next: NextFunction) => {
-    if (!ObjectId.isValid(req.params.blogId)) {
-        res.status(404).send("Incorrect BlogId,  please enter a valid one")
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(404).send("Incorrect id,  please enter a valid one")
     } else {
         next()
     }
