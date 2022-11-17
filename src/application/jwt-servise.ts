a0f62669da1a1f66e38623519c93304534dfc944
Alexsandr3 <fam_86@mail.ru>
2022-11-17 19:14:37 +0100

    hometask 09 add limeter fix jwt

--- src/application/jwt-servise.ts
+++ src/application/jwt-servise.ts
@@ -6,12 +6,12 @@ import {DeviceDBType} from "../types/device_types";
 
 export const jwtService = {
     async createJwt(device: DeviceDBType) {
-        const accessToken = jwt.sign({userId: device.userId}, settings.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+        const accessToken = jwt.sign({userId: device.userId}, settings.ACCESS_TOKEN_SECRET, {expiresIn: '10s'})
         const refreshToken = jwt.sign({
             userId: device.userId,
             deviceId: device.deviceId,
             lastActiveDate: device.lastActiveDate,
-        }, settings.REFRESH_TOKEN_SECRET, {expiresIn: '1h'})
+        }, settings.REFRESH_TOKEN_SECRET, {expiresIn: '10s'})
         const returnedTokens: TokensType = {
             accessToken,
             refreshToken
