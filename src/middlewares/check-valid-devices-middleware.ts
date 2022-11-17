ef3f9a93a2274433ebddecb668f0ecee378d9b90
Alexsandr3 <fam_86@mail.ru>
2022-11-17 23:34:20 +0100

    hometask 09 fix 010

--- src/middlewares/check-valid-devices-middleware.ts
+++ src/middlewares/check-valid-devices-middleware.ts
@@ -14,8 +14,8 @@ export const checkValidDeviceMiddleware = async (req: Request, res: Response, ne
     const dateExp = new Date(payload.exp)
     if (dateExp < new Date()) return res.sendStatus(HTTP_STATUSES.FORBIDDEN_403)
   //  if (payload.deviceId !== req.params.id) return res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-    const user = await deviceRepositories.findDeviceByUserId(payload.userId)
-    if (payload.userId !== user?.userId) return res.sendStatus(HTTP_STATUSES.FORBIDDEN_403)
+   // const user = await deviceRepositories.findDeviceByUserId(payload.userId)
+   // if (payload.userId !== user?.userId) return res.sendStatus(HTTP_STATUSES.FORBIDDEN_403)
   //  req.payload = payload
     next()
 }
