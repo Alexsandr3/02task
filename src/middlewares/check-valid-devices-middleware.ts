bfacf3008a713f7f58be1c4343ff993cb5401b80
Alexsandr3 <fam_86@mail.ru>
2022-11-18 00:02:16 +0100

    hometask 09 fix 012 with log

--- src/middlewares/check-valid-devices-middleware.ts
+++ src/middlewares/check-valid-devices-middleware.ts
@@ -6,15 +6,19 @@ import {deviceRepositories} from "../repositories/device-db-repositories";
 
 export const validationInputMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.cookies.refreshToken
+    console.log('000-refreshToken----',refreshToken)
     if (!refreshToken) return res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
     if (!req.params.id) return res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
     const isValidDeviceId = await deviceRepositories.findDeviceByDeviceId(req.params.id)
+    console.log('001-isValidDeviceId----',isValidDeviceId)
     if (!isValidDeviceId) return res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
     const payload = await jwtService.verifyToken(refreshToken)
+    console.log('002-payload----',payload)
    // const dateExp = new Date(payload.exp)
    // if (dateExp < new Date()) return res.sendStatus(HTTP_STATUSES.FORBIDDEN_403)
   //  if (payload.deviceId !== req.params.id) return res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
     const user = await deviceRepositories.findDeviceByUserId(payload.userId)
+    console.log('003-user----',user)
     if (payload.userId !== user?.userId) return res.sendStatus(HTTP_STATUSES.FORBIDDEN_403)
   //  req.payload = payload
     next()
