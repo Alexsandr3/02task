10940e9dd0327664f9489885df118325c5c3a4e3
Alexsandr3 <fam_86@mail.ru>
2022-11-18 20:14:25 +0100

    hometask 09 fix 033

--- src/middlewares/check-refresh-id-tokena.ts
+++ src/middlewares/check-refresh-id-tokena.ts
@@ -4,14 +4,9 @@ import {jwtService} from "../application/jwt-servise";
 import {deviceRepositories} from "../repositories/device-db-repositories";
 
 
-export const checkRefreshAndIdTokena = async (req: Request, res: Response, next: NextFunction) => {
+export const checkAccess = async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.cookies.refreshToken
-    if (!refreshToken) return res.status(HTTP_STATUSES.UNAUTHORIZED_401).send("Did not come refreshToken")
     const payload = await jwtService.verifyToken(refreshToken)
-
-    const dateExp = new Date(payload.exp * 1000)
-    if (dateExp < new Date()) return res.status(HTTP_STATUSES.UNAUTHORIZED_401).send('Expired date')
-
     const deviceUser = await deviceRepositories.findDeviceForValid(payload.userId, payload.deviceId, payload.iat)
     if(!deviceUser) return res.status(HTTP_STATUSES.UNAUTHORIZED_401).send('Incorrect userId or deviceId or issuedAt')
     if(deviceUser.deviceId !== req.params.id) return res.status(HTTP_STATUSES.FORBIDDEN_403).send('Incorrect deviceIdt')
