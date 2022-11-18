d69c1b197abe2b0954a5a396f29326becdef4a2b
Alexsandr3 <fam_86@mail.ru>
2022-11-18 11:46:21 +0100

    hometask 09 fix 021 with log

--- src/middlewares/check-valid-devices-middleware.ts
+++ src/middlewares/check-valid-devices-middleware.ts
@@ -12,11 +12,11 @@ export const validationInputMiddleware = async (req: Request, res: Response, nex
     console.log('001-isValidDeviceId----',isValidDeviceId)
     if (!isValidDeviceId) return res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
     const payload = await jwtService.verifyToken(refreshToken)
-    if (payload.deviceId !== req.params.id) return res.sendStatus(HTTP_STATUSES.FORBIDDEN_403)
     console.log('002-payload----',payload)
     const dateExp = new Date(payload.exp * 1000)
     console.log('003-dateExp----',dateExp)
     console.log('004-newDate----',new Date())
+    if (payload.deviceId !== req.params.id) return res.sendStatus(HTTP_STATUSES.FORBIDDEN_403)
     if (dateExp < new Date()) return res.sendStatus(HTTP_STATUSES.FORBIDDEN_403) //????
     const user = await deviceRepositories.findDeviceByUserId(payload.userId)
     console.log('005-user----',user)
