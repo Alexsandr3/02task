60a636b9a669b47e231b19f224cdb667780fdfe6
Alexsandr3 <fam_86@mail.ru>
2022-11-18 19:30:46 +0100

    hometask 09 fix 032

--- src/middlewares/auth_login-validation-middleware.ts
+++ src/middlewares/auth_login-validation-middleware.ts
@@ -2,22 +2,22 @@ import {body} from "express-validator";
 import {inputValidetionsMiddleware} from "./Input-validetions-middleware";
 
 
-const loginValidation =
-    body('login',
-        'login must be a string, must not be empty, length must be between 3 and 10 characters')
+const loginOrEmailValidation =
+    body('loginOrEmail',
+        'loginOrEmail must be a string, must not be empty')
         .isString()
         .notEmpty()
         .trim()
 const passwordValidation =
     body('password',
-        'password must be a string, must not be empty, length must be between 6 and 20 characters')
+        'password must be a string, must not be empty')
         .isString()
         .notEmpty()
         .trim()
 
 
 export const loginValidations = [
-    loginValidation,
+    loginOrEmailValidation,
     passwordValidation,
     inputValidetionsMiddleware
 ]
